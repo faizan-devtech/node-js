a6073ad04c917f179b40d755e4c64c18b0d98bc3
faizan abbasi <faizan.devtech@gmail.com>
2026-04-23 10:20:49 -0700

    git

--- colors.js
+++ colors.js
@@ -1,3 +1,8 @@
-const colors=require('colors')
+const colors=require('colors');
+//sendign http request to server //
+const http=require('http');
+http.request((req,res)=>{
+    res.end('hello world');
+})
 console.log(colors.red('my name is faizan abbasi'));
 console.log(colors.green('my name is faizan abbasi'));
