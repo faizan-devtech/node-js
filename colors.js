d72f4bd03171ad37a2c12168b887e75f9f05c7a7
faizan abbasi <faizan.devtech@gmail.com>
2026-04-23 10:03:44 -0700

    Initial commit of backend code

--- colors.js
+++ colors.js
@@ -1,2 +1,3 @@
 const colors=require('colors')
-console.log(colors.red('my name is faizan abbasi'))
+console.log(colors.red('my name is faizan abbasi'));
+console.log(colors.green('my name is faizan abbasi'));
